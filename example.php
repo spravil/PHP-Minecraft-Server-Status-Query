1f1d2c0fc71f882db36e25f0fd17670695e52cd0
Julian Spravil <julian.spr@t-online.de>
2014-01-15 19:47:17 +0100

    Minecraft 1.7.* support

--- example.php
+++ example.php
@@ -2,11 +2,12 @@
 
 	include_once 'MinecraftServerStatus/status.class.php';
 	$status = new MinecraftServerStatus();
-	$response = $status->getStatus('pvp24.com');
+	$response = $status->getStatus('Mad-Gamble.net');
 	if(!$response) {
 		echo"The Server is offline!";
 	} else {
-		echo"The Server ".$response['hostname']." is running on ".$response['version']." and is online,
+		echo"<img width=\"64\" height=\"64\" src=\"".$response['favicon']."\" /> <br>
+		The Server ".$response['hostname']." is running on ".$response['version']." and is online,
 		currently are ".$response['players']." players online
 		of a maximum of ".$response['maxplayers'].". The motd of the server is '".$response['motd']."'. 
 		The server has a ping of ".$response['ping']." milliseconds.";
