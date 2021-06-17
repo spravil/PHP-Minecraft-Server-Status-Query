9e930317181728a3ea341f95df2185f29a5816f9
Julian Spravil <979130+spravil@users.noreply.github.com>
2021-06-17 18:01:28 +0200

    Update HandshakePacket.php

--- src/Packets/HandshakePacket.php
+++ src/Packets/HandshakePacket.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Minecraft Server Status Query
- *
- * @link        https://github.com/FunnyItsElmo/PHP-Minecraft-Server-Status-Query/
- * @author      Julian Spravil <julian.spr@t-online.de>
- * @copyright   Copyright (c) 2016 Julian Spravil
- * @license     https://github.com/FunnyItsElmo/PHP-Minecraft-Server-Status-Query/blob/master/LICENSE
- */
 namespace MinecraftServerStatus\Packets;
 
 class HandshakePacket extends Packet {
